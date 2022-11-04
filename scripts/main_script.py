03c23c1b1f8fe3e01edc690267a91215019692f1
Daniel García Angulo <>
2022-11-04 20:59:30 +0100

    Crear script principal

---
+++ scripts/main_script.py
@@ -0,0 +1,16 @@
+# loading data
+print('task_1')
+print('task_1')
+print('task_1')
+print('task_1')
+
+# Preprocessing
+print('task_1')
+print('task_1')
+print('task_1')
+
+# create nico's map
+print('task_2')
+print('task_2')
+print('task_2')
+print('task_2')
